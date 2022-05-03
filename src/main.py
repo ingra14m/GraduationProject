0600b6e5c8e79129da5cd8283ec37fc1da04d7b3
ingra14m <1261238025@qq.com>
2022-05-03 20:51:28 +0800

    Use softmax to Add AUC

--- src/main.py
+++ src/main.py
@@ -90,9 +90,9 @@ def train(model, graph, optimizer, output, add_self_loop=False):
                     f1_score(result_label[train_mask], result_pred[train_mask], average='weighted'),
                     f1_score(result_label[val_mask], result_pred[val_mask], average='weighted'),
                     f1_score(result_label[test_mask], result_pred[test_mask], average='weighted'),
-                    roc_auc_score(result_label[train_mask], result_pred[train_mask]),
-                    roc_auc_score(result_label[val_mask], result_pred[val_mask]),
-                    roc_auc_score(result_label[test_mask], result_pred[test_mask]),
+                    roc_auc_score(result_label[train_mask], result_pred[train_mask], multi_class='ovr'),
+                    roc_auc_score(result_label[val_mask], result_pred[val_mask], multi_class='ovr'),
+                    roc_auc_score(result_label[test_mask], result_pred[test_mask], multi_class='ovr'),
                 )
                 print(content)
                 print(quality)
